f910c81df32f47c908eccd4a44b7fe4d73bc19f3
Andrew Mockett <califormula@gmail.com>
2015-10-19 11:40:50 -0600

    heroku again

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: 'charliedog',
   cookie: {secure: false}
 }));
 
