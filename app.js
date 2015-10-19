280a269ae0437d71474a6921f852dc2acca5cfe9
Andrew Mockett <califormula@gmail.com>
2015-10-19 11:39:16 -0600

    i hate heroku

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({
-  secret: 'charliedog',
+  secret: process.env.SESSION_SECRET,
   cookie: {secure: false}
 }));
 
